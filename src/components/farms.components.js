63c53bdc98d51b1221e8917adbd8a3fdbd530939
Ellyn Liu <47698191+ellynliu@users.noreply.github.com>
2020-06-24 15:30:02 -0500

    create farms page

---
+++ src/components/farms.components.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Farms extends Component {
+  render() {
+    return (
+      <div>
+        <p>List all farms on this page</p>
+      </div>
+    )
+  }
+}
