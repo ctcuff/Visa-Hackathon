1de3895500deae0a3145a2a52466b8431bb6b7de
Ellyn Liu <47698191+ellynliu@users.noreply.github.com>
2020-06-24 15:24:30 -0500

    fixed typo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
-import Home from "./components.home.component";
+import Home from "./components/home.component";
 import About from "./components/about.component";
 import Shop from "./components/shop.component";
 
