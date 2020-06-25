1aaab12974c48be9219a4ef34c2a397d45720350
Ellyn Liu <ellynliu@Ellyns-MacBook-Pro.local>
2020-06-25 09:40:47 -0500

    reverted a commit

---
+++ src/components/account.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Account extends Component {
+  render() {
+    return (
+      <div>
+        <p>Account Username: </p>
+        <p>Account Password: </p>
+      </div>
+    )
+  }
+}
