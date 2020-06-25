8fc3ce27044ae94e3822caade1f739c6efa89ff7
Ellyn Liu <ellynliu@Ellyns-MacBook-Pro.local>
2020-06-25 14:22:29 -0500

    added another form to manage items

--- src/components/farmer.manage.component.js
+++ src/components/farmer.manage.component.js
@@ -12,7 +12,10 @@ export default class FarmerManage extends Component {
     this.state = {
       itemname: '',
       price: '0.00',
-      category: ''
+      category: 'vegetable',
+      newPrice: '0.00',
+      stock: 'in stock',
+      remove: 'keep'
     }
   }
 
@@ -34,19 +37,81 @@ export default class FarmerManage extends Component {
     })
   }
 
+  handleStockChange = event => {
+    this.setState({
+      stock: event.target.value
+    })
+  }
+
+  handleNewPriceChange = event => {
+    this.setState({
+      newPrice: event.target.value
+    })
+  }
+
+  handleRemoveChange = event => {
+    this.setState({
+      remove: event.target.value
+    })
+  }
+
   // POST request can use this information
-  handleSubmit = event => {
-    alert(`${this.state.itemname} ${this.state.price} ${this.state.category}`)
+  handleSubmitAdd = event => {
+    alert(`Name: ${this.state.itemname}\nPrice: ${this.state.price}\nCategory: ${this.state.category}`)
     // this prevents the form from resetting text after submission
+    event.preventDefault()
+  }
+
+  handleSubmitEdit = event => {
+    alert(`Stock: ${this.state.stock}\nNew Price: ${this.state.newPrice}\nKeep or remove: ${this.state.remove}`)
     event.preventDefault()
   }
 
 
   render() {
     return (
-      <div>
-        <h1>Add Item</h1>
-        <form onSubmit={this.handleSubmit}>
+      <div className="container">
+        <h1>Manage Items</h1>
+
+        <form onSubmit={this.handleSubmitEdit}>
+        <div>
+        <h3>View and Change Items</h3>
+          <p>display each item here after querying</p>
+
+            <div>
+              <label>In Stock/Out of Stock</label>
+              <select value={this.state.stock} onChange={this.handleStockChange} >
+                <option value="in stock">In Stock</option>
+                <option value="out of stock">Out of Stock</option>
+              </select>
+            </div>
+
+            <div>
+              <label>Change Price</label>
+              <input
+                type='text' pattern='^\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$'
+                value={this.state.newPrice}
+                onChange={this.handleNewPriceChange}
+                />
+            </div>
+
+            <div>
+              <label>Remove Item</label>
+              <select value={this.state.remove} onChange={this.handleRemoveChange} >
+                <option value="keep">Keep</option>
+                <option value="remove">Remove</option>
+              </select>
+            </div>
+
+            <button type="submit">Submit</button>
+        </div>
+        </form>
+
+
+
+
+        <h3>Add Item</h3>
+        <form onSubmit={this.handleSubmitAdd}>
           <div>
             <label>Item name</label>
             <input
