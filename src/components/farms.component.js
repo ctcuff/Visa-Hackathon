561041f89bafbb8dde6a4398832c393c182c9ee2
Jeffrey <huangjef@uchicago.edu>
2020-06-25 17:11:31 -0400

    Possible Nearby Farms Representation

--- src/components/farms.component.js
+++ src/components/farms.component.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import { Carousel } from 'react-bootstrap';
+import Farm1 from '../assets/Farm1.jpeg';
+import Farm2 from '../assets/Farm2.jpg';
+import Farm3 from '../assets/Farm3.jpg';
 
 export default class Farms extends Component {
   render() {
     return (
       <div>
-        <p>List all farms on this page</p>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={Farm1}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Farm Number One</h3>
+              <p>Distance: 10 miles, Main Products: Blueberries</p>
+              <a href="/"> Browse Products</a>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={Farm2}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Farm Number Two</h3>
+              <p>Distance: 12 miles, Main Products: Milk</p>
+              <a href="/"> Browse Products</a>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={Farm3}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Farm Number Three</h3>
+              <p>Distance: 30 miles, Main Products: Strawberries</p>
+              <a href="/"> Browse Products</a>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
     )
   }
