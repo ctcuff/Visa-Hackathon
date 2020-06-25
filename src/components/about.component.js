a97e616f74a8b704ddf0946c17adbc41ba08f34d
Ellyn Liu <ellynliu@Ellyns-MacBook-Pro.local>
2020-06-25 16:30:22 -0500

    about page

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -1,11 +1,157 @@
-import React, { Component } from 'react';
+import React from "react";
+import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from "mdbreact";
 
-export default class About extends Component {
-  render() {
-    return (
-      <div>
-        <p>About this website</p>
-      </div>
-    )
-  }
+const TeamPage = () => {
+  return (
+    <MDBCard className="my-5 px-5 pb-5 text-center">
+        <MDBCardBody>
+          <h2 className="h1-responsive font-weight-bold my-5">
+            Our Team
+          </h2>
+          <p className="grey-text w-responsive mx-auto mb-5">
+            Summary
+          </p>
+          <MDBRow>
+            <MDBCol>
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/55fc0004e4b069a519961e2d/1442590746571-RPGKIXWGOO671REUNMCB/ke17ZwdGBToddI8pDm48kKVo6eXXpUnmuNsFtLxYNDVZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7abfyk2s94xLLkDA7TSo2rckMlGDU48FfF-V7lLcSuGNU_Uf7d6wOiJwP-LWX64gbQ/image-asset.gif"
+                className="rounded-circle z-depth-1 img-fluid"
+                alt="Sample avatar"
+              />
+              <h5 className="font-weight-bold mt-4 mb-3">Monica Boomgaarden</h5>
+              <p className="blue-text">Year and School</p>
+              <p className="grey-text">
+                Other information
+              </p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="facebook-f" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="instagram" className="blue-text" />
+                </a>
+              </ul>
+            </MDBCol>
+
+            <MDBCol>
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/55fc0004e4b069a519961e2d/1442590746571-RPGKIXWGOO671REUNMCB/ke17ZwdGBToddI8pDm48kKVo6eXXpUnmuNsFtLxYNDVZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7abfyk2s94xLLkDA7TSo2rckMlGDU48FfF-V7lLcSuGNU_Uf7d6wOiJwP-LWX64gbQ/image-asset.gif"
+                className="rounded-circle z-depth-1 img-fluid"
+                alt="Sample avatar"
+              />
+              <h5 className="font-weight-bold mt-4 mb-3">Alec Chen</h5>
+              <p className="blue-text">Year and School</p>
+              <p className="grey-text">
+                Other information
+              </p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="facebook-f" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="instagram" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="dribbble" className="blue-text" />
+                </a>
+              </ul>
+            </MDBCol>
+
+            <MDBCol>
+              <img
+                tag="img"
+                src="https://images.squarespace-cdn.com/content/v1/55fc0004e4b069a519961e2d/1442590746571-RPGKIXWGOO671REUNMCB/ke17ZwdGBToddI8pDm48kKVo6eXXpUnmuNsFtLxYNDVZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7abfyk2s94xLLkDA7TSo2rckMlGDU48FfF-V7lLcSuGNU_Uf7d6wOiJwP-LWX64gbQ/image-asset.gif"
+                className="rounded-circle z-depth-1 img-fluid"
+                alt="Sample avatar"
+              />
+              <h5 className="font-weight-bold mt-4 mb-3">Cameron Cuff</h5>
+              <p className="blue-text">Year and School</p>
+              <p className="grey-text">
+                Other information
+              </p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="facebook-f" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="github" className="blue-text" />
+                </a>
+              </ul>
+            </MDBCol>
+          </MDBRow>
+
+          <MDBRow>
+            <MDBCol>
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/55fc0004e4b069a519961e2d/1442590746571-RPGKIXWGOO671REUNMCB/ke17ZwdGBToddI8pDm48kKVo6eXXpUnmuNsFtLxYNDVZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7abfyk2s94xLLkDA7TSo2rckMlGDU48FfF-V7lLcSuGNU_Uf7d6wOiJwP-LWX64gbQ/image-asset.gif"
+                className="rounded-circle z-depth-1 img-fluid"
+                alt="Sample avatar"
+              />
+              <h5 className="font-weight-bold mt-4 mb-3">Jeffrey Huang</h5>
+              <p className="blue-text">Year and School</p>
+              <p className="grey-text">
+                Other information
+              </p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="facebook-f" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="instagram" className="blue-text" />
+                </a>
+              </ul>
+            </MDBCol>
+
+            <MDBCol>
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/55fc0004e4b069a519961e2d/1442590746571-RPGKIXWGOO671REUNMCB/ke17ZwdGBToddI8pDm48kKVo6eXXpUnmuNsFtLxYNDVZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7abfyk2s94xLLkDA7TSo2rckMlGDU48FfF-V7lLcSuGNU_Uf7d6wOiJwP-LWX64gbQ/image-asset.gif"
+                className="rounded-circle z-depth-1 img-fluid"
+                alt="Sample avatar"
+              />
+              <h5 className="font-weight-bold mt-4 mb-3">Ellyn Liu</h5>
+              <p className="blue-text">Year and School</p>
+              <p className="grey-text">
+                Other information
+              </p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="facebook-f" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="instagram" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="dribbble" className="blue-text" />
+                </a>
+              </ul>
+            </MDBCol>
+
+            <MDBCol>
+              <img
+                tag="img"
+                src="https://images.squarespace-cdn.com/content/v1/55fc0004e4b069a519961e2d/1442590746571-RPGKIXWGOO671REUNMCB/ke17ZwdGBToddI8pDm48kKVo6eXXpUnmuNsFtLxYNDVZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7abfyk2s94xLLkDA7TSo2rckMlGDU48FfF-V7lLcSuGNU_Uf7d6wOiJwP-LWX64gbQ/image-asset.gif"
+                className="rounded-circle z-depth-1 img-fluid"
+                alt="Sample avatar"
+              />
+              <h5 className="font-weight-bold mt-4 mb-3">Nicolas Molano</h5>
+              <p className="blue-text">Year and School</p>
+              <p className="grey-text">
+                Other information
+              </p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="facebook-f" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="github" className="blue-text" />
+                </a>
+              </ul>
+            </MDBCol>
+          </MDBRow>
+
+        </MDBCardBody>
+      </MDBCard>
+  );
 }
+
+export default TeamPage;
