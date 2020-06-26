1ad9d8abb42b86a7f75e19abaee1905dd6645828
ctcuff <dev.ctcuff@gmail.com>
2020-06-26 17:26:17 -0400

    Added Visa Checkout API

--- src/components/payment.component.js
+++ src/components/payment.component.js
@@ -5,6 +5,12 @@ export default class Payment extends Component {
     return (
       <div>
         <p>Visa Checkout BUtton</p>
+        <img 
+          alt="Visa Checkout" 
+          class="v-button" 
+          role="button" 
+          src="https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png"
+          />
       </div>
     )
   }
