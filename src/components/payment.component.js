743c4dc38a728360525fa29c2fa84955f4cf598a
Jeffrey <huangjef@uchicago.edu>
2020-06-26 12:12:48 -0400

    Merchant Item Table

--- src/components/payment.component.js
+++ src/components/payment.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Shop extends Component {
+export default class Payment extends Component {
   render() {
     return (
       <div>
--- src/components/shop.component.js
+++ src/components/shop.component.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const products = [];
+for (let i = 0; i < 46; i++) {
+  products.push({
+    id: i, 
+    name: 'Strawberries', 
+    price: '0.50', 
+    farm: 'Bob ${i}', 
+    status: 'in stock'
+  });
+}
+
+const columns = [{
+  dataField: 'id',
+  text: 'Product ID'
+}, {
+  dataField: 'name',
+  text: 'Product Name'
+}, {
+  dataField: 'price',
+  text: 'Product Price'
+}, {
+  dataField: 'farm',
+  text: 'Local Farm'
+}, {
+  dataField: 'status',
+  text: 'Product Status'
+}
+];
 
 export default class Shop extends Component {
+  state = {
+    selectedRowKeys: [], // Check here to configure the default column
+    loading: false,
+  };
+
+  start = () => {
+    this.setState({ loading: true });
+    // ajax request after empty completing
+    setTimeout(() => {
+      this.setState({
+        selectedRowKeys: [],
+        loading: false,
+      });
+    }, 1000);
+  };
+
+  onSelectChange = selectedRowKeys => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    this.setState({ selectedRowKeys });
+  };
+
   render() {
     return (
       <div>
         <p>Browse for groceries</p>
+        <BootstrapTable keyField='id' data={ products } columns={ columns } />
       </div>
     )
   }
