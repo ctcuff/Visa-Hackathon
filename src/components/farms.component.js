f46a1516b0d9473ba40a8a7aadb2c05c2e7e5add
ctcuff <dev.ctcuff@gmail.com>
2020-06-26 21:12:50 -0400

    Vendors are now displayed from the database

--- src/components/farms.component.js
+++ src/components/farms.component.js
@@ -1,55 +1,50 @@
 import '../styles/farms.css';
 import React, { Component } from 'react';
 import { Carousel } from 'react-bootstrap';
+import axios from 'axios';
 import Farm1 from '../assets/Farm1.jpeg';
 import Farm2 from '../assets/Farm2.jpg';
 import Farm3 from '../assets/Farm3.jpg';
 
+const placeHolderImages = [Farm1, Farm2, Farm3];
+
 export default class Farms extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      vendors: []
+    };
+  }
+  
+  componentDidMount() {
+    axios.get('http://localhost:5000/vendors')
+      .then(response => {
+        this.setState({ vendors: response.data });
+      })
+      .catch(err => console.error(err))
+  }
+
   render() {
     return (
       <div>
         <Carousel>
-          <Carousel.Item>
-            <img
-              className="farm-carousel-img"
-              src={Farm1}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>Farm Number One</h3>
-              <p>Distance: 10 miles, Main Products: Blueberries</p>
-              <a href="/"> Browse Products</a>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="farm-carousel-img"
-              src={Farm2}
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Farm Number Two</h3>
-              <p>Distance: 12 miles, Main Products: Milk</p>
-              <a href="/"> Browse Products</a>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="farm-carousel-img"
-              src={Farm3}
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Farm Number Three</h3>
-              <p>Distance: 30 miles, Main Products: Strawberries</p>
-              <a href="/"> Browse Products</a>
-            </Carousel.Caption>
-          </Carousel.Item>
+        {this.state.vendors.map((vendor, index) => (
+          <Carousel.Item key={index}>
+              <img 
+                className="farm-carousel-img" 
+                src={placeHolderImages[index % placeHolderImages.length]}
+                alt={vendor.name}
+              />
+              <Carousel.Caption>
+                <h3>{vendor.company}</h3>
+                <p>{vendor.address.city}, {vendor.address.state}</p>
+                <a href="/">Browse Products</a>
+              </Carousel.Caption>
+            </Carousel.Item>
+        ))}
         </Carousel>
       </div>
-    )
+    );
   }
 }
