8887d1f792adf6ee88c3da19879713462d31b117
Ellyn Liu <ellynliu@Ellyns-MacBook-Pro.local>
2020-06-29 15:50:45 -0500

    added tabs for item manager

--- src/components/farmer.manage.component.js
+++ src/components/farmer.manage.component.js
@@ -1,69 +1,40 @@
 import React, { Component } from 'react';
 import { BootstrapTable , TableHeaderColumn, DeleteButton } from 'react-bootstrap-table';
 import axios from 'axios';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+// Change farmer name to the username of the vendor
+const farmerName = 'johnapple'
 
 const cellEditProp = {
   mode: 'dbclick'
 };
-
-const columns = [{
-  dataField: 'id',
-  text: 'Product ID',
-  sort: true,
-  sortFunc: (a, b, order, dataField, rowA, rowB) => {
-    if (order === 'asc') {
-      return b - a;
-    }
-    return a - b; // desc
-  }
-}, {
-  dataField: 'name',
-  text: 'Product Name',
-  sort: true
-}, {
-  dataField: 'category',
-  text: 'Category',
-  sort: true
-}, {
-  dataField: 'price',
-  text: 'Product Price',
-  sort: true
-}, {
-  dataField: 'inStock',
-  text: 'In Stock'
-}];
-
-let order = 'desc';
 
 const selectRowProp = {
   mode: 'checkbox',
   clickToSelect: true  // enable click to select
 };
 
-
 export default class FarmerManage extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       products: [],
-      itemname: '',
+      item: '',
       price: '0.00',
       category: 'vegetable',
-      newPrice: '0.00',
-      stock: true,
-      remove: 'keep'
+      inStock: true,
     }
   }
 
-  componentDidMount() {
-    console.log("hello2")
+  fetchItems() {
     axios.get('http://localhost:5000/items')
     .then(res => {
 
       const filteredProducts = res.data.filter(entry => {
-        return entry.vendorUsername == 'johnapple';
+        return entry.vendorUsername == farmerName;
       });
 
       this.setState({
@@ -75,30 +46,8 @@ export default class FarmerManage extends Component {
     })
   }
 
-  state = {
-    selectedRowKeys: [], // Check here to configure the default column
-    loading: false,
-  };
-
-  start = () => {
-     this.setState({ loading: true });
-     // ajax request after empty completing
-     setTimeout(() => {
-       this.setState({
-         selectedRowKeys: [],
-         loading: false,
-       });
-     }, 1000);
-   };
-
-   onSelectChange = selectedRowKeys => {
-     console.log('selectedRowKeys changed: ', selectedRowKeys);
-     this.setState({ selectedRowKeys });
-   };
-
-
-  getInitialState() {
-    return ({price: "0.00"});
+  componentDidMount() {
+    this.fetchItems()
   }
 
   handleItemnameChange = event => {
@@ -124,23 +73,14 @@ export default class FarmerManage extends Component {
       stock: event.target.value
     })
   }
-
+/*
   handleNewPriceChange = event => {
     this.setState({
       newPrice: event.target.value
     })
   }
-
-  handleRemoveChange = event => {
-    this.setState({
-      remove: event.target.value
-    })
-  }
-
+*/
   handleDeleteButtonClick = (onClick) => {
-    // Custom your onClick event here,
-    // it's not necessary to implement this function if you have no any process before onClick
-    console.log('This is my custom function for DeleteButton click event');
     onClick();
   }
 
@@ -155,11 +95,11 @@ export default class FarmerManage extends Component {
     );
   }
 
-  // POST request can use this information
+  // Allow the vendor to add an item
   handleSubmitAdd = event => {
     event.preventDefault()
     const databody = {
-      vendorUsername: "johnapple",
+      vendorUsername: farmerName,
       price: this.state.price,
       category: this.state.category,
       item: this.state.itemname,
@@ -169,6 +109,7 @@ export default class FarmerManage extends Component {
     axios.post('http://localhost:5000/items/create', databody)
       .then((res) => {
         console.log(res.data)
+        this.fetchItems()
       }).catch((error) => {
         console.log(error)
       });
@@ -176,13 +117,13 @@ export default class FarmerManage extends Component {
     alert(`Name: ${this.state.itemname}\nPrice: ${this.state.price}\nCategory: ${this.state.category}`)
   }
 
+  // Allow vendor to make changes to the item name, category, price, and change whether or not the item is in stock
   handleSubmitEdit = event => {
-    alert(`Stock: ${this.state.stock}\nNew Price: ${this.state.newPrice}\nKeep or remove: ${this.state.remove}`)
+    alert(`Stock: ${this.state.stock}\nNew Price: ${this.state.newPrice}\nIn Stock: ${this.state.inStock}`)
     event.preventDefault()
   }
 
   render() {
-
     const options = {
       deleteBtn: this.createCustomDeleteButton
     };
@@ -193,52 +134,52 @@ export default class FarmerManage extends Component {
     return (
       <div className="container">
         <h1>Manage Items</h1>
-
-        <div>
-        <h3>View and Change Items</h3>
-
-          <div>
-            <p>Browse for groceries</p>
-            <BootstrapTable selectRow={selectRow} data={ this.state.products } options={options} pagination cellEdit={cellEditProp} deleteRow>
-              <TableHeaderColumn dataField='item' isKey={true} dataSort={ true }>Product Name</TableHeaderColumn>
-              <TableHeaderColumn dataField='category' dataSort={ true }>Category</TableHeaderColumn>
-              <TableHeaderColumn dataField='price' dataSort={ true }>Product Price</TableHeaderColumn>
-              <TableHeaderColumn dataField='inStock' dataSort={ true }>In Stock</TableHeaderColumn>
-            </BootstrapTable>
-          </div>
-        </div>
-
-        <h3>Add Item</h3>
-        <form onSubmit={this.handleSubmitAdd}>
-          <div>
-            <label>Item name</label>
-            <input
-              type='text'
-              value={this.state.itemname}
-              onChange={this.handleItemnameChange}
-              />
-          </div>
-
-          <div>
-            <label>Price</label>
-            <input
-              type='text' pattern='^\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$'
-              value={this.state.price}
-              onChange={this.handlePriceChange}
-              />
-          </div>
-
-          <div>
-            <label>Category</label>
-            <select value={this.state.category} onChange={this.handleCategoryChange} >
-              <option value="vegetable">Vegetable</option>
-              <option value="fruit">Fruit</option>
-              <option value="dairy">Dairy</option>
-              <option value="meat">Meat</option>
-            </select>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+        <Tabs>
+          <TabList>
+            <Tab>View and Change Items</Tab>
+            <Tab>Add Items</Tab>
+          </TabList>
+            <TabPanel>
+              <div>
+                <BootstrapTable selectRow={selectRow} data={ this.state.products } options={options} pagination cellEdit={cellEditProp} deleteRow>
+                  <TableHeaderColumn dataField='item' isKey={true} dataSort={ true }>Product Name</TableHeaderColumn>
+                  <TableHeaderColumn dataField='category' dataSort={ true }>Category</TableHeaderColumn>
+                  <TableHeaderColumn dataField='price' dataSort={ true }>Product Price</TableHeaderColumn>
+                  <TableHeaderColumn dataField='inStock' dataSort={ true }>In Stock</TableHeaderColumn>
+                </BootstrapTable>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <form onSubmit={this.handleSubmitAdd}>
+                <div>
+                  <label>Item name</label>
+                  <input
+                    type='text'
+                    value={this.state.itemname}
+                    onChange={this.handleItemnameChange}
+                    />
+                </div>
+                <div>
+                  <label>Price</label>
+                  <input
+                    type='text' pattern='^\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$'
+                    value={this.state.price}
+                    onChange={this.handlePriceChange}
+                    />
+                </div>
+                <div>
+                  <label>Category</label>
+                  <select value={this.state.category} onChange={this.handleCategoryChange} >
+                    <option value="vegetable">Vegetable</option>
+                    <option value="fruit">Fruit</option>
+                    <option value="dairy">Dairy</option>
+                    <option value="meat">Meat</option>
+                  </select>
+                </div>
+                <button type="submit">Submit</button>
+              </form>
+            </TabPanel>
+        </Tabs>
       </div>
     )
   }
