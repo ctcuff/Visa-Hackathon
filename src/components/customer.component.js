b0778f729f25e47f9760a040eaf67ee53bff5168
ctcuff <dev.ctcuff@gmail.com>
2020-07-01 12:24:29 -0400

    Added functionality for customer/merchant login/sign-up

--- src/components/customer.component.js
+++ src/components/customer.component.js
@@ -1,50 +1,93 @@
 import '../styles/customer.css';
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { useHistory } from "react-router-dom";
+import User from '../util/user';
 
 export default class Customer extends Component {
   constructor(props) {
     super(props);
-    this.customer = React.createRef();
-    this.address = React.createRef();
-    this.email = React.createRef();
-    this.phone = React.createRef();
+    this.usernameRef = React.createRef();
+    this.passwordRef = React.createRef();
+    this.state = {
+      isLoading: false,
+      validated: false
+    };
   }
 
   addCustomer = (event) => {
     event.preventDefault();
-    alert("You are submitting " + this.customer.current.value +
-    this.address.current.value +
-    this.email.current.value +
-    this.phone.current.value);
+    const username = this.usernameRef.current.value.trim();
+    const password = this.passwordRef.current.value.trim();
+
+    if (!username || !password) {
+      this.setState({ validated: true });
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    const postData = {
+      username,
+      password
+    };
+
+    const data = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postData)
+    };
+
+    fetch('http://localhost:5000/customers/signUp', data)
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          alert('Error: ' + res.error);
+          return;
+        }
+
+        User.save(username, User.CUSTOMER);
+        alert('Account created. Press OK to finish');
+        window.location = '/';
+      })
+      .catch(err => alert('Error: ' + err))
+      .finally(() => this.setState({ isLoading: false }));
   }
 
   render() {
     return (
       <div className="container customer">
-        <Form>
+        <h1>Customer Sign Up</h1>
+        <Form noValidate validated={this.state.validated}>
         <Form.Group controlId="formCustomer">
-          <Form.Label>Customer Name</Form.Label>
-          <Form.Control type="text" placeholder="First, Last" ref={this.customer}/>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Username" required ref={this.usernameRef}/>
+          <Form.Control.Feedback type="invalid">
+              Please enter a username
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="formAddress">
-          <Form.Label>Street Address</Form.Label>
-          <Form.Control type="address" placeholder="123 River Drive" ref = {this.address}/>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" required ref={this.passwordRef}/>
+          <Form.Control.Feedback type="invalid">
+              Please enter a password
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email Contact</Form.Label>
-          <Form.Control type="email" placeholder="example@some-site.com" ref={this.email}/>
-        </Form.Group>
-        <Form.Group controlId="formPhone">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="tel" placeholder="123-123-1234" ref={this.phone}/>
-        </Form.Group>
-        </Form>
-        <Button href="/account" onClick={this.addCustomer} className="customer-sign-up-btn">
-          Sign Up
+        <Button 
+          onClick={this.addCustomer} 
+          className="customer-sign-up-btn"
+          disabled={this.state.isLoading}
+        >
+          {this.state.isLoading ? (
+            <div>
+              <Spinner animation="border" size="sm" />
+              {' '} Loading...
+            </div>
+          ) : 'Sign Up'}
         </Button>
+        </Form>
         <div className="customer-login">
           <span>Already have an account? {' '}</span>
           <Link to="/account">Login</Link>
--- src/components/merchant.component.js
+++ src/components/merchant.component.js
@@ -2,66 +2,150 @@ import '../styles/merchant.css';
 import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import User from '../util/user';
 
 export default class Merchant extends Component {
   constructor(props) {
     super(props);
-    this.merchant = React.createRef();
-    this.business = React.createRef();
-    this.address = React.createRef();
-    this.email = React.createRef();
-    this.phone = React.createRef();
-    this.username = React.createRef();
-    this.password = React.createRef();
-    this.picture = React.createRef();
+
+    this.usernameRef = React.createRef();
+    this.passwordRef = React.createRef();
+    this.companyRef = React.createRef();
+    this.streetAddressRef = React.createRef();
+    this.cityRef = React.createRef();
+    this.stateRef = React.createRef();
+    this.zipCodeRef = React.createRef();
+
+    this.state = {
+      isLoading: false,
+      validated: false
+    };
   }
-  
+
   addMerchant = (event) => {
     event.preventDefault();
-    alert("You are submitting " + this.merchant.current.value +
-    this.business.current.value +
-    this.address.current.value +
-    this.email.current.value +
-    this.phone.current.value);
+    const refs = [
+      this.usernameRef,
+      this.passwordRef,
+      this.companyRef,
+      this.streetAddressRef,
+      this.cityRef,
+      this.stateRef
+    ];
+
+    // Check every input field to make sure each field
+    // has a value
+    if (!refs.every(ref => !!ref.current.value)) {
+      this.setState({ validated: true });
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    const body = {
+      username: this.usernameRef.current.value,
+      password: this.passwordRef.current.value,
+      company: this.companyRef.current.value,
+      address: {
+        street: this.streetAddressRef.current.value,
+        city: this.cityRef.current.value,
+        state: this.stateRef.current.value,
+        zipcode: this.zipCodeRef.current.value
+      }
+    }
+
+    const postData = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    };
+
+    fetch('http://localhost:5000/vendors/signUp', postData)
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          alert(`Error: ${res.error}`);
+          return;
+        }
+
+        alert('Account successfully created. Press OK to finish');
+        User.save(body.username, User.MERCHANT);
+        window.location = '/';
+      })
+      .catch(err => console.log(err))
+      .finally(() => this.setState({ isLoading: false }));
   }
 
   render() {
     return (
       <div className="container merchant">
-        <Form>
-        <Form.Group controlId="formMerchant">
-          <Form.Label>Merchant Name</Form.Label>
-          <Form.Control type="text" placeholder="First, Last" ref={this.merchant}/>
-        </Form.Group>
-        <Form.Group controlId="formBusiness">
-          <Form.Label>Business</Form.Label>
-          <Form.Control type="text" placeholder="Enter Business" ref={this.business}/>
-        </Form.Group>
-        <Form.Group controlId="formAddress">
-          <Form.Label>Street Address</Form.Label>
-          <Form.Control type="address" placeholder="123 River Drive" ref = {this.address}/>
-        </Form.Group>
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email Contact</Form.Label>
-          <Form.Control type="email" placeholder="example@some-site.com" ref={this.email}/>
-        </Form.Group>
-        <Form.Group controlId="formPhone">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="tel" placeholder="123-123-1234" ref={this.phone}/>
-        </Form.Group>
-        <Form.Group controlId="formUsername">
+        <h1> Merchant Sign Up</h1>
+        <Form noValidate validated={this.state.validated}>
+
+        <Form.Group>
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="" ref={this.username}/>
+          <Form.Control required type="text" placeholder="Username" ref={this.usernameRef}/>
+          <Form.Control.Feedback type="invalid">
+              Please enter a username
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group controlId="formPassword">
+
+        <Form.Group>
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="" ref={this.password}/>
+          <Form.Control required type="password" placeholder="Password" ref={this.passwordRef}/>
+          <Form.Control.Feedback type="invalid">
+            Please enter a password
+          </Form.Control.Feedback>
         </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Company Name</Form.Label>
+          <Form.Control required type="text" placeholder="Company Name" ref={this.companyRef}/>
+          <Form.Control.Feedback type="invalid">
+            Please enter a company name
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Street Address</Form.Label>
+          <Form.Control required type="address" placeholder="123 River Drive" ref = {this.streetAddressRef}/>
+          <Form.Control.Feedback type="invalid">
+            Please enter a street address
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>City</Form.Label>
+          <Form.Control required type="text" placeholder="Miami" ref={this.cityRef}/>
+          <Form.Control.Feedback type="invalid">
+            Please enter a city
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>State</Form.Label>
+          <Form.Control required type="text" placeholder="FL" ref={this.stateRef}/>
+          <Form.Control.Feedback type="invalid">
+            Please enter a state
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Zip Code</Form.Label>
+          <Form.Control required type="number" placeholder="33101" ref={this.zipCodeRef}/>
+          <Form.Control.Feedback type="invalid">
+            Please enter a zip code
+          </Form.Control.Feedback>
+        </Form.Group>
+
         <Form.Group>
           <Form.File id="exampleFormControlFile1" label="Upload Farm Image" />
         </Form.Group>
+
         </Form>
-        <Button href="/account" onClick={this.addMerchant}>
+        <Button onClick={this.addMerchant}>
           Sign Up
         </Button>
         <div className="merchant-login">
