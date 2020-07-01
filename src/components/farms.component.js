4e84b51248b88f2b64b79a47bb4d35864722020f
Jeffrey <huangjef@uchicago.edu>
2020-07-01 15:52:17 -0400

    Added post functionality

--- src/components/farms.component.js
+++ src/components/farms.component.js
@@ -1,11 +1,12 @@
 import '../styles/farms.css';
 import React, { Component } from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Button, Carousel, Form, FormControl } from 'react-bootstrap';
 import axios from 'axios';
 import Farm1 from '../assets/Farm1.jpeg';
 import Farm2 from '../assets/Farm2.jpg';
 import Farm3 from '../assets/Farm3.jpg';
 import { Link } from 'react-router-dom';
+import RangeSlider from 'react-bootstrap-range-slider';
 
 
 const placeHolderImages = [Farm1, Farm2, Farm3];
@@ -16,13 +17,59 @@ export default class Farms extends Component {
 
     this.state = {
       vendors: [],
+      radius: 50,
+      searchValue: '',
       currentLocation: {
         lat: 0.0,
         lng: 0.0
       }
     };
   }
+
+  handleSearchValueChange = (event) => {
+    this.setState({ searchValue: event.target.value });
+  };
+
+  handleRadiusValueChange = (event) => {
+    this.setState({ radius: event.target.value });
+  };
   
+  queryMerchants = (event) => {
+    event.preventDefault();
+    const latitude = this.state.currentLocation.lat;
+    const longitude = this.state.currentLocation.lng;
+    const search = this.state.searchValue;
+    const radius = this.state.radius;
+
+    const postData = {
+      latitude, 
+      longitude, 
+      search, 
+      radius
+    };
+
+    console.log(postData)
+    // const data = {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(postData)
+    // };
+
+    axios.post('http://localhost:5000/locater/', postData)
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          alert('Error: ' + res.error);
+          return;
+        }
+        console.log('hi')
+        console.log(res)
+      })
+      .catch(err => alert('Error: ' + err));
+  }
+
   browseFarmer(name) {
     return {pathname: '/shop', searchProps: { searchTerm: name}}
   }
@@ -53,6 +100,19 @@ export default class Farms extends Component {
       <div>
         <p> Latitude: {this.state.currentLocation.lat} </p> 
         <p> Longitude: {this.state.currentLocation.lng} </p> 
+        <Form inline onSubmit={this.queryMerchants}>
+          <Form.Group> 
+            <RangeSlider
+              value={Number(this.state.radius)}
+              onChange={this.handleRadiusValueChange}
+              tooltipPlacement='top'
+              min={20}
+              max={100}
+            />
+          </Form.Group>
+          <FormControl type="text" placeholder="Franchise Example" className="mr-sm-2" onChange={this.handleSearchValueChange} />
+          <Button variant="outline-success" onClick={this.queryMerchants}>Search</Button>
+        </Form>
         <Carousel>
         {this.state.vendors.map((vendor, index) => (
           <Carousel.Item key={index}>
