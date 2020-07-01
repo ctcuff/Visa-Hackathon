d6bfbeea4ec3bc4839fd4eb8c577729da3b1c9f2
Ellyn Liu <ellynliu@Ellyns-MacBook-Pro.local>
2020-07-01 11:26:10 -0500

    orders css

--- src/components/orders.component.js
+++ src/components/orders.component.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import '../styles/payment.css';
+import '../styles/orders.css';
 
 export default class Orders extends Component {
   render() {
     return (
-      <div>
-        <h1>Orders Page</h1>
+      <div class="container">
+        <h1 className="orders-heading">Orders Page</h1>
       </div>
     )
   }
