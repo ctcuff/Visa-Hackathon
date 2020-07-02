f65e4cf941419b58aa416c60d67021e29cb5f725
ctcuff <dev.ctcuff@gmail.com>
2020-07-01 23:09:28 -0400

    Fixed 404 errors for order routes

--- backend/orderRouter.js
+++ backend/orderRouter.js
@@ -10,7 +10,7 @@ orderRouter.get('/', async (req, res) => {
     });
 });
 
-orderRouter.get('vendor/:vendorUsername', async (req, res) => {
+orderRouter.get('/vendor/:vendorUsername', async (req, res) => {
     Order.find({vendorUsername: req.params.vendorUsername }, function (err, data) {
       if(err) throw err;
       console.log(data);
@@ -19,7 +19,7 @@ orderRouter.get('vendor/:vendorUsername', async (req, res) => {
     });
 });
 
-orderRouter.get('customer/:customerUsername', async (req, res) => {
+orderRouter.get('/customer/:customerUsername', async (req, res) => {
     Order.find({customerUsername: req.params.customerUsername }, function (err, data) {
       if(err) throw err;
       console.log(data);
