440adecac9641d04c68f6cde580dddc0f455a58f
Jeffrey <huangjef@uchicago.edu>
2020-07-02 00:21:59 -0400

    Post Changes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import '../styles/item.css';
 import { Link } from "react-router-dom";
+import StarRatings from 'react-star-ratings';
+import { Button, Form } from 'react-bootstrap';
 
 export default class Post extends Component {
     constructor(props){
         super(props);
+    }
+
+    changeRating( newRating, name ) {
+      alert("Rating functionality yet to come! You rated product: " + newRating + " stars.")
+    }
+    
+    onComment = (event) => {
+      event.preventDefault();
+      alert("Comment functionality yet to come! We will make sure to let our farmers know what you think!")
     }
     render() {
         const order = this.props.order;
@@ -13,7 +24,8 @@ export default class Post extends Component {
         const image = this.props.image;
         const farm = this.props.farm;
         const status = this.props.status;
-
+        const randomstars = (Math.random() * (5.00))
+        console.log(randomstars)
         return (
           <div classname = "Product"> 
             <article className="Post" ref="Post">
@@ -30,7 +42,7 @@ export default class Post extends Component {
                 <h2 className="Post-status" style={{color: status === true ? "green" : "red"}}> 
                   In Stock: {status} 
                 </h2>
-                <p class="price"> ${price} </p>
+                <p className="price"> ${price} </p>
                 
                 <div className="Post-image"> 
                   <div className="Post-image-bg">
@@ -39,6 +51,24 @@ export default class Post extends Component {
                 </div>
                 <div className="Post-caption">
                   <strong> {name}</strong>
+                </div>
+                <div className="Post-stars">
+                  <StarRatings
+                    rating={randomstars}
+                    starRatedColor="blue"
+                    changeRating={this.changeRating}
+                    numberOfStars={5}
+                    name='rating'
+                  />
+                </div>
+                <div className="Post-comment">
+                <Form> 
+                  <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Leave a comment!</Form.Label>
+                    <Form.Control as="textarea" rows="3" />
+                  </Form.Group>
+                  <Button onClick={this.onComment}> Submit </Button>
+                </Form>
                 </div>
             </article>
           </div>
